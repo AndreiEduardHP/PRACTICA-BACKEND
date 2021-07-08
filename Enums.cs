aad18de2f8e36705d87fe71dcfc99deea67a3b83
AndreiEduardHP <72997357+AndreiEduardHP@users.noreply.github.com>
2021-07-08 13:53:50 +0300

    Add files via upload

---
+++ Enums.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HONEV
+{
+    public class Enums
+    {
+        public enum UserSortType
+        {
+            FirstNameAscendent,     //0
+            FirstNameDescendent,    //1
+            LastNameAscendent,      //2
+            LastNameDescendent,     //3
+        }
+
+        public enum ProductSortType
+        {
+            PriceAscendent,
+            PriceDescendent,
+            NameAscendent,
+            NameDescendent,
+        }
+    }
+}
